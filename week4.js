4bfefa69b04fc123867e55339d36c559842ef841
yashveeMavani <117334937+yashveeMavani@users.noreply.github.com>
2023-01-08 21:01:48 +0530

    Add files via upload

---
+++ week4.js
@@ -0,0 +1,25 @@
+console.log("demmo");
+function audi() {
+  let x1 = {
+    name: "Audi",
+    model: "XYZ",
+  };
+  document.getElementById("msg").textContent = x1.name;
+  document.getElementById("msg2").textContent = x1.model;
+}
+function maruti() {
+  let X2 = {
+    name: "maruti",
+    model: "ABC",
+  };
+  document.getElementById("msg").textContent = X2.name;
+  document.getElementById("msg2").textContent = X2.model;
+}
+function Marcedes() {
+  let X3 = {
+    name: "Marcedes",
+    model: "PQR",
+  };
+  document.getElementById("msg").textContent = X3.name;
+  document.getElementById("msg2").textContent = X3.model;
+}
